1ca28e9b77042be0cf79bfab9ae4aabf371cadfd
Patrick Zhang <patrickzhangyf@hotmail.com>
2018-11-22 02:07:58 -0500

    make board

--- src/App.js
+++ src/App.js
@@ -3,23 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      board: Array(9).fill(null),
+      player: null,
+      winner: null
+    }
+  }
+
+  handleClick = (position) => {
+    console.log("clicked in position ", position)
+  }
+
   render() {
+    const squares = this.state.board.map(square => <div className="square" onClick={this.handleClick}>{square}</div>)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <h1>Tic Tac Toe</h1>
+        <div className="board">
+          {squares}
+        </div>
       </div>
     );
   }
